931ca90b29a881d86f382558b36ea6256c0bc161
Tidian Delage <tdelage@1G5.42angouleme.fr>
2025-05-02 01:47:53 +0200

    started working on random number generation

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: tdelage <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 08:52:06 by tdelage           #+#    #+#             */
-/*   Updated: 2025/01/28 15:48:58 by tdelage          ###   ########.fr       */
+/*   Updated: 2025/05/02 01:28:57 by tdelage          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,14 @@
 # include <ft_ascii.h>
 # include <ft_assert.h>
 # include <ft_cstr.h>
+# include <ft_time.h>
 # include <io/files.h>
 # include <io/streams.h>
 # include <logger.h>
 # include <memory.h>
+# include <random.h>
 # include <strings.h>
 # include <system/calls/syscalls.h>
-# include <ft_time.h>
 # include <types.h>
 
 #endif // LIBFT_H
--- includes/random.h
+++ includes/random.h
@@ -1,23 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   random.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tdelage <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/02 01:40:21 by tdelage           #+#    #+#             */
+/*   Updated: 2025/05/02 01:40:21 by tdelage          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef RANDOM_H
 # define RANDOM_H
 
 # include <strings.h>
 # include <types.h>
 
-void		ft_srand(t_u64 seed);
-t_u64		ft_rand(t_u64 a, t_u64 b);
+typedef t_u64	t_seed;
 
-t_u8		rand_next_u8(void);
-t_u16		rand_next_u16(void);
-t_u32		rand_next_u32(void);
-t_u64		rand_next_u64(void);
+void			ft_srand(t_seed seed);
+t_u64			ft_urand(t_u64 a, t_u64 b);
+t_i64			ft_irand(t_i64 a, t_i64 b);
+t_u64			ft_surand(t_seed seed, t_u64 a, t_u64 b);
+t_i64			ft_sirand(t_seed seed, t_i64 a, t_i64 b);
 
-t_i8		rand_next_i8(void);
-t_i16		rand_next_i16(void);
-t_i32		rand_next_i32(void);
-t_i64		rand_next_i64(void);
+typedef t_u64	(*t_urand_f)(t_any, t_u64, t_u64);
+typedef t_i64	(*t_irand_f)(t_any, t_i64, t_i64);
+typedef void	(*t_srand_f)(t_any, t_u64);
 
-t_string	rand_next_string(t_u64 len);
-bool		rand_coinflip(void);
+typedef struct s_random
+{
+	t_urand_f	urand;
+	t_irand_f	irand;
+	t_srand_f	srand;
+	t_any		data;
+}				t_random;
+
+t_random		random_get_default(void);
+t_random		random_get_unique(t_seed seed);
+
+void			random_set_seed(t_random *rand, t_seed seed);
+t_u64			random_unsigned_value(t_random *rand, t_u64 a, t_u64 b);
+t_i64			random_signed_value(t_random *rand, t_i64 a, t_i64 b);
+
+t_u8			random_next_u8(t_random *rand);
+t_u16			random_next_u16(t_random *rand);
+t_u32			random_next_u32(t_random *rand);
+t_u64			random_next_u64(t_random *rand);
+
+t_i8			random_next_i8(t_random *rand);
+t_i16			random_next_i16(t_random *rand);
+t_i32			random_next_i32(t_random *rand);
+t_i64			random_next_i64(t_random *rand);
+
+t_f32			random_next_f32(t_random *rand);
+t_f64			random_next_f64(t_random *rand);
+t_f128			random_next_f128(t_random *rand);
+
+t_string		random_next_string(t_random *rand, t_u64 len);
+bool			random_coin_flip(t_random *rand);
 
 #endif // RANDOM_H
