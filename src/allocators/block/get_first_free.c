582ff2c16e57139586cdeeb51cb854669851f086
Tidian Delage <tdelage@1G5.42angouleme.fr>
2025-05-02 01:46:15 +0200

    fixing destructor function for memory free

--- src/allocators/block/get_first_free.c
+++ src/allocators/block/get_first_free.c
@@ -6,7 +6,7 @@
 /*   By: tdelage <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 10:12:03 by tdelage           #+#    #+#             */
-/*   Updated: 2024/10/30 12:40:28 by tdelage          ###   ########.fr       */
+/*   Updated: 2025/05/02 01:42:45 by tdelage          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 void	__attribute__((destructor))	destroy_block_arena(void)
 {
-	t_arena	*arena;
+	t_arena		*arena;
 
 	arena = block_arena_static_storage();
 	arena_free(arena);
