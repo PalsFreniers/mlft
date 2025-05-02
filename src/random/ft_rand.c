9fba753c622c16a6e0db9f91b977957b038e38bc
Tidian Delage <tdelage@1G5.42angouleme.fr>
2025-05-02 02:25:52 +0200

    rand function working

--- src/random/ft_rand.c
+++ src/random/ft_rand.c
@@ -6,16 +6,20 @@
 /*   By: tdelage <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 01:39:47 by tdelage           #+#    #+#             */
-/*   Updated: 2025/05/02 01:40:07 by tdelage          ###   ########.fr       */
+/*   Updated: 2025/05/02 01:59:38 by tdelage          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-void	ft_rand(bool gen, t_seed seed, t_u64 a, t_u64 b)
+#define RANDOM_MULT_VALUE 6364136223846793005ULL
+
+t_u64	ft_rand(bool gen, t_seed seed)
 {
 	static t_seed	__static_mlft_seed = 123456;
 
 	if (!gen)
-		return ((void)(__static_mlft_seed = seed));
+		return (__static_mlft_seed = seed);
+	seed = RANDOM_MULT_VALUE * seed + 1;
+	return (seed >> 33);
 }
